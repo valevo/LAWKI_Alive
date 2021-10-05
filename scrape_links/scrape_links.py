adbf3f92df927d345d1c170fb511fe81a7191c62
valevo <valevogelmann@gmx.net>
2021-10-05 12:57:56 +0200

    first upload of scrape_links

---
+++ scrape_links/scrape_links.py
@@ -0,0 +1,63 @@
+from tqdm import tqdm
+import joblib
+
+import pandas as pd
+import numpy.random as rand
+
+from time import sleep
+import re
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+
+import os
+os.environ['MOZ_HEADLESS'] = '1'
+
+from utils import get_links, load, platforms
+
+###
+
+top_langs = ["en", "zh-cn", "hi", "es", "ar", "bn", "fr", "ru", "pt", "ur", "de", "jp", "id"]
+n = -1
+lang_d = {l: load(f"terms/{l}.txt", n) for l in top_langs}
+    
+trans_d = {}
+for i, t in enumerate(lang_d["en"]):
+    trans_d[t] = {l: lang_d[l][i] for l in lang_d}
+    
+
+    
+###
+
+
+records = []
+
+driver = webdriver.Firefox()
+
+num_links = 5
+
+for p, f in platforms.items():
+    progressbar = tqdm(trans_d.items(), desc=f"platform={p}")
+    for topic, term_d in progressbar:
+        for lang, term in term_d.items():
+            progressbar.set_description(f"topic={topic}, lang={lang}")
+            links = get_links(platform=p, query=f, n=1, term=term, driver=driver)
+            for l in links[:num_links]:
+                records.append(
+                    dict(
+                         platform=p,
+                         language=lang,
+                         topic=topic,
+                         term=term,
+                         link=l
+                    )
+                )
+                
+    joblib.dump(records, f"records_{p}.pkl")
+    
+###
+
+
+df = pd.DataFrame.from_records(records)
+df.to_csv("links.csv", line_terminator="\n", quotechar='"', quoting=csv.QUOTE_ALL)
---
+++ scrape_links/utils.py
@@ -0,0 +1,111 @@
+import os
+import time
+import requests
+
+from bs4 import BeautifulSoup as bs4
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+from urllib.parse import quote as url_quote
+import re
+from time import sleep
+
+
+
+platforms = {"youtube": lambda s: f"https://www.youtube.com/results?search_query={s}&sp=EgYIBBABGAE%3D",
+            "dailymotion": lambda s: f"https://www.dailymotion.com/search/{s}/videos?duration=mins_1_5&dateRange=past_month"}
+
+youtube_regex = r'watch\?v=(.+?)"'
+dailymotion_regex = r'href="/video/(.+?)"'
+
+
+
+def load(f, n=-1):
+    with open(f, encoding="utf8") as handle:
+        return [l.strip() for l in handle if l.strip()][:n]
+
+
+def remove_duplicates(ls):
+    s = set()
+    for x in ls:
+        if not x in s:
+            s.add(x)
+            yield x
+
+            
+def click_button_if_exists(driver, do_quick_check=True):
+    if do_quick_check:
+        pg = driver.page_source
+        if re.search("VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc IIdkle",
+         pg) is None: return False
+    
+    slct = "#yDmH0d > c-wiz > div > div > div > div.NIoIEf > div.G4njw > div.qqtRac > form > div.lssxud > div > button"
+    try:
+        submit_button = driver.find_element_by_css_selector(slct)
+        submit_button.click()
+        print("button clicked!")
+        return True
+    except NoSuchElementException:
+        return False
+
+
+def request_and_scroll(url, num_scrolls=1, driver=None, is_youtube=False):
+    if not driver:
+#         driver = webdriver.Firefox()
+        pass
+        
+    driver.get(url)
+    if is_youtube:
+        clicked = click_button_if_exists(driver, do_quick_check=True)
+        
+    for i in range(num_scrolls):
+        driver.execute_script("window.scrollTo(1,5000)") # 5000000
+        sleep(1)
+
+    page = str(driver.page_source)
+        
+#     driver.close()
+    
+    return page
+
+######################################
+
+
+def check_no_results(pg, platform):
+    if platform == "youtube":
+        return find_video_links(pg, r'watch\?v=(.+?)"') == []
+    elif platform == "dailymotion":
+        return re.search("Search for something else or remove search filters.", pg) is not None
+
+
+def find_video_links(page, regex):
+    find_video_urls = re.compile(regex)
+    
+    try:
+        return list(remove_duplicates(m.group(1) for m in re.finditer(regex, str(page))))
+    except IndexError:
+        print(regex, [m.start() for m in re.finditer(regex, str(page))])
+
+#######################################
+        
+        
+def get_links(platform=None, query=None, term=None, n=10, driver=None):
+    url = query(url_quote(term))
+    result_page = request_and_scroll(query(term), num_scrolls=n, driver=driver, 
+                                     is_youtube=(platform == "youtube"))
+    
+    
+    if platform == "youtube":
+        r = youtube_regex
+    elif platform == "dailymotion":
+        r = dailymotion_regex
+    else:
+        raise ValueError(f"given platform {platform} is not defined!")
+    
+    if check_no_results(result_page, platform):
+        return [None]
+    
+    
+    return find_video_links(result_page, r)
